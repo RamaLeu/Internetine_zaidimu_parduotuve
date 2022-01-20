cc87f2be36a317def0f3d245f8519393d84a8487
Ramaleu <ramunas8250@gmail.com>
2022-01-21 00:18:30 +0200

    initial commit

---
+++ index/index.js
@@ -0,0 +1,71 @@
+
+let translate = 0
+
+let prev = document.querySelector(".prevBtn")
+let next =  document.querySelector(".nextBtn")
+
+prev.addEventListener("click", function() {
+    if (translate > 0){
+        translate -= 33
+        if (translate == 1){
+            translate = 0
+        }
+        document.querySelector(".inner").style.transform = `translateX(${-translate}%)`;
+    }
+});
+next.addEventListener("click", function(){
+    if (translate < 66){
+        translate += 33
+        if (translate == 99){
+            translate = 100
+        }
+        document.querySelector(".inner").style.transform = `translateX(${-translate}%)`;
+    }
+})
+
+
+let prev2 = document.querySelector(".prevBtn2")
+let next2 = document.querySelector(".nextBtn2")
+let translate2 = 0
+
+prev2.addEventListener("click", function() {
+    if (translate2 > 0){
+        translate2 -= 50
+        if (translate2 == 1){
+            translate2 = 0
+        }
+        document.querySelector(".inner2").style.transform = `translateX(${-translate2}%)`;
+    }
+});
+next2.addEventListener("click", function(){
+    if (translate2 < 50){
+        translate2 += 50
+        if (translate2 == 99){
+            translate2 = 100
+        }
+        document.querySelector(".inner2").style.transform = `translateX(${-translate2}%)`;
+    }
+})
+
+let prev3 = document.querySelector(".prevBtn3")
+let next3 = document.querySelector(".nextBtn3")
+let translate3 = 0
+
+prev3.addEventListener("click", function() {
+    if (translate3 > 0){
+        translate3 -= 50
+        if (translate3 == 1){
+            translate3 = 0
+        }
+        document.querySelector(".inner3").style.transform = `translateX(${-translate3}%)`;
+    }
+});
+next3.addEventListener("click", function(){
+    if (translate3 < 50){
+        translate3 += 50
+        if (translate3 == 99){
+            translate3 = 100
+        }
+        document.querySelector(".inner3").style.transform = `translateX(${-translate3}%)`;
+    }
+})
