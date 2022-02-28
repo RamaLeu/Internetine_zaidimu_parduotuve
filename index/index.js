8f52da3ef4e110130c01a44b18ce7121a9eb81a5
Aura Špejeraitė <ArSpej@gmail.com>
2022-02-28 22:34:03 +0200

    some changes for form scrolling and design

--- index/index.js
+++ index/index.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword,  signInWithEmailAndPassword , onAuthStateChanged, signOut, setPersistence, browserSessionPersistence } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js";
-import { getDatabase, ref, set  } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, setPersistence, browserSessionPersistence } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyDA9Ez4i7vSWvq8uzvmmy8CMQ54x-EDRfs",
-databaseURL: "https://derrastore-default-rtdb.europe-west1.firebasedatabase.app/",
-authDomain: "derrastore.firebaseapp.com",
-projectId: "derrastore",
-storageBucket: "derrastore.appspot.com",
-messagingSenderId: "70390486239",
-appId: "1:70390486239:web:f0e884d634f8114597856a"
+  apiKey: "AIzaSyDA9Ez4i7vSWvq8uzvmmy8CMQ54x-EDRfs",
+  databaseURL: "https://derrastore-default-rtdb.europe-west1.firebasedatabase.app/",
+  authDomain: "derrastore.firebaseapp.com",
+  projectId: "derrastore",
+  storageBucket: "derrastore.appspot.com",
+  messagingSenderId: "70390486239",
+  appId: "1:70390486239:web:f0e884d634f8114597856a"
 };
 
 let currentUser
 let register = false
-const app = initializeApp(firebaseConfig); 
+const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth();
 
@@ -35,42 +35,42 @@ setPersistence(auth, browserSessionPersistence)
 
 //-Rasymas i duombaze kuri yra different nuo authentication duombazes, su papildomais duomenis
 function writeUserData(data) {
-    console.log(currentUser)
-    const db = getDatabase();
-    set(ref(db, 'users/' + data.id), {
-      username: data.username,
-      admin: data.admin,
-      email: data.email,
-      surname: data.surname,
-      country: data.country,
-    });
-  }
+  console.log(currentUser)
+  const db = getDatabase();
+  set(ref(db, 'users/' + data.id), {
+    username: data.username,
+    admin: data.admin,
+    email: data.email,
+    surname: data.surname,
+    country: data.country,
+  });
+}
 
 let userData = {
-    "id": "",
-    "admin": true,
-    "username": "",
-    "surname": "",
-    "email": "",
-    "country": "",
-    "password": ""
+  "id": "",
+  "admin": true,
+  "username": "",
+  "surname": "",
+  "email": "",
+  "country": "",
+  "password": ""
 }
 
 //- Checkinimas, jei registracijos metu kuris nors input field yra empty
-function checkIfEmpty(err, value){
-    console.log(`Value is ${value}`)
-    if (value == "" /* || value == "----" */ || value == false){
-        console.log("WRONG");
-        errList.push(err)
-        return false;
-    }
-    else{
-        return true;
-    }
+function checkIfEmpty(err, value) {
+  console.log(`Value is ${value}`)
+  if (value == "" /* || value == "----" */ || value == false) {
+    console.log("WRONG");
+    errList.push(err)
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 
-document.querySelector("#signUpSubmit").addEventListener("click", function(e){
+document.querySelector("#signUpSubmit").addEventListener("click", function (e) {
   e.preventDefault()
   let result = true
   document.querySelector(".errs").innerHTML = "";
@@ -82,43 +82,43 @@ document.querySelector("#signUpSubmit").addEventListener("click", function(e){
   result = checkIfEmpty("<p>Repeat your password</p><br/>", document.querySelector("#repeat-password").value)
   result = checkIfEmpty("<p>You must agree to our privacy policy</p><br/>", document.querySelector("#agreement").checked)
 
-  if (document.querySelector("#reg-password").value == document.querySelector("#repeat-password").value){
+  if (document.querySelector("#reg-password").value == document.querySelector("#repeat-password").value) {
   }
-  else{
-      result = false;
-      errList.push("Passwords must match")
+  else {
+    result = false;
+    errList.push("Passwords must match")
   }
 
-  for (let item in errList){
-      errs.innerHTML += errList[item];
+  for (let item in errList) {
+    errs.innerHTML += errList[item];
   }
   errList = []
 
   console.log(result)
 
-  if (result){
-      register = true;
-      userData.username = document.querySelector("#reg-username").value;
-      userData.surname = document.querySelector("#reg-surname").value;
-      userData.email = document.querySelector("#reg-email").value;
-      userData.country = document.querySelector("#reg-country").value;
-      userData.password = document.querySelector("#reg-password").value;
-      createUserWithEmailAndPassword(auth, userData.email, userData.password)
+  if (result) {
+    register = true;
+    userData.username = document.querySelector("#reg-username").value;
+    userData.surname = document.querySelector("#reg-surname").value;
+    userData.email = document.querySelector("#reg-email").value;
+    userData.country = document.querySelector("#reg-country").value;
+    userData.password = document.querySelector("#reg-password").value;
+    createUserWithEmailAndPassword(auth, userData.email, userData.password)
       .then((userCredential) => {
-          // Signed in 
-          const user = userCredential.user;
-          // ...
+        // Signed in 
+        const user = userCredential.user;
+        // ...
       })
       .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // ..
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
       });
 
-      //writeUserData(userData) 
-      switchPage(0);
+    //writeUserData(userData) 
+    switchPage(0);
   }
-  
+
 })
 
 //register dalies pabaiga
@@ -130,15 +130,15 @@ onAuthStateChanged(auth, (user) => {
   if (user) {
     const uid = user.uid;
     console.log(uid)
-    if (register){
+    if (register) {
       userData.id = uid;
       writeUserData(userData);
       register = false
     }
-    currentUser = user 
+    currentUser = user
     document.querySelector('.regBtns').innerHTML = `<li>${currentUser.email}</li>
     <a href="#" class="logoutNav"><li>LOGOUT</li></a>`;
-    document.querySelector('.logoutNav').addEventListener("click", function(e){
+    document.querySelector('.logoutNav').addEventListener("click", function (e) {
       e.preventDefault()
       logout()
     });
@@ -146,39 +146,39 @@ onAuthStateChanged(auth, (user) => {
   } else {
     document.querySelector('.regBtns').innerHTML = `<a href="#" id="loginTag"><li>Login</li></a>
     <a href="#" id="registerTag"><li>Sign up</li></a>`;
-    document.querySelector("#registerTag").addEventListener("click", function(){
+    document.querySelector("#registerTag").addEventListener("click", function () {
       switchPage(2);
     });
-    document.querySelector("#loginTag").addEventListener("click", function(){
+    document.querySelector("#loginTag").addEventListener("click", function () {
       switchPage(1);
     });
   }
-  });
-  
+});
+
 
 //login dalis
 
 
 //-Prisijungimas prie sistemos
-function login(){
+function login() {
   let email = document.querySelector("#log-email").value
   let password = document.querySelector("#log-password").value
 
   signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    switchPage(0);
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      switchPage(0);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 
 }
 
 //-Atsijungimas nuo sistemos
-function logout(){
+function logout() {
   signOut(auth).then(() => {
     console.log("signed out")
     currentUser = null
@@ -189,8 +189,8 @@ function logout(){
 
 
 //-Consoleje ismeta esama user
-function checkUser(){
-  if (currentUser){
+function checkUser() {
+  if (currentUser) {
     const uid = currentUser.uid;
     console.log(uid)
     console.log(currentUser.email)
@@ -199,19 +199,19 @@ function checkUser(){
 
 }
 
-document.querySelector("#signInSubmit").addEventListener("click", function(e){
+document.querySelector("#signInSubmit").addEventListener("click", function (e) {
   e.preventDefault();
   login();
 })
-document.querySelector('#logout').addEventListener("click", function(e){
-e.preventDefault();
-logout();
-})
+// document.querySelector('#logout').addEventListener("click", function (e) {
+//   e.preventDefault();
+//   logout();
+// })
 
-document.querySelector('#checkUser').addEventListener("click", function(e){
-e.preventDefault();
-checkUser();
-})
+// document.querySelector('#checkUser').addEventListener("click", function (e) {
+//   e.preventDefault();
+//   checkUser();
+// })
 
 
 //login dalies pabaiga
@@ -220,105 +220,105 @@ checkUser();
 let translate = 0;
 
 let prev = document.querySelector(".prevBtn");
-let next =  document.querySelector(".nextBtn");
+let next = document.querySelector(".nextBtn");
 
-prev.addEventListener("click", function() {
-    if (translate > 0){
-        translate -= 25;
-        document.querySelector(".inner").style.transform = `translateX(${-translate}%)`;
-    }
+prev.addEventListener("click", function () {
+  if (translate > 0) {
+    translate -= 25;
+    document.querySelector(".inner").style.transform = `translateX(${-translate}%)`;
+  }
 });
-next.addEventListener("click", function(){
-    if (translate < 75){
-        translate += 25;
-        document.querySelector(".inner").style.transform = `translateX(${-translate}%)`;
-    }
+next.addEventListener("click", function () {
+  if (translate < 75) {
+    translate += 25;
+    document.querySelector(".inner").style.transform = `translateX(${-translate}%)`;
+  }
 })
 
 let prev2 = document.querySelector(".prevBtn2");
 let next2 = document.querySelector(".nextBtn2");
 let translate2 = 0;
 
-prev2.addEventListener("click", function() {
-    if (translate2 > 0){
-        translate2 -= 50;
-        if (translate2 == 1){
-            translate2 = 0;
-        }
-        document.querySelector(".inner2").style.transform = `translateX(${-translate2}%)`;
+prev2.addEventListener("click", function () {
+  if (translate2 > 0) {
+    translate2 -= 50;
+    if (translate2 == 1) {
+      translate2 = 0;
     }
+    document.querySelector(".inner2").style.transform = `translateX(${-translate2}%)`;
+  }
 });
-next2.addEventListener("click", function(){
-    if (translate2 < 50){
-        translate2 += 50;
-        if (translate2 == 99){
-            translate2 = 100;
-        };
-        document.querySelector(".inner2").style.transform = `translateX(${-translate2}%)`;
-    }
+next2.addEventListener("click", function () {
+  if (translate2 < 50) {
+    translate2 += 50;
+    if (translate2 == 99) {
+      translate2 = 100;
+    };
+    document.querySelector(".inner2").style.transform = `translateX(${-translate2}%)`;
+  }
 })
 
 let prev3 = document.querySelector(".prevBtn3");
 let next3 = document.querySelector(".nextBtn3");
 let translate3 = 0;
 
-prev3.addEventListener("click", function() {
-    if (translate3 > 0){
-        translate3 -= 50;
-        if (translate3 == 1){
-            translate3 = 0;
-        }
-        document.querySelector(".inner3").style.transform = `translateX(${-translate3}%)`;
+prev3.addEventListener("click", function () {
+  if (translate3 > 0) {
+    translate3 -= 50;
+    if (translate3 == 1) {
+      translate3 = 0;
     }
+    document.querySelector(".inner3").style.transform = `translateX(${-translate3}%)`;
+  }
 });
-next3.addEventListener("click", function(){
-    if (translate3 < 50){
-        translate3 += 50;
-        if (translate3 == 99){
-            translate3 = 100;
-        }
-        document.querySelector(".inner3").style.transform = `translateX(${-translate3}%)`;
+next3.addEventListener("click", function () {
+  if (translate3 < 50) {
+    translate3 += 50;
+    if (translate3 == 99) {
+      translate3 = 100;
     }
+    document.querySelector(".inner3").style.transform = `translateX(${-translate3}%)`;
+  }
 })
 
 //Carouseles scripto pabaiga
 //Puslapio keitimas
 
-function switchPage(page){
-  if (page == 0){
+function switchPage(page) {
+  if (page == 0) {
     switchingFunction('registerPage', false);
     switchingFunction('loginPage', false);
     switchingFunction('mainPage', true);
   }
-  else if(page == 1){
+  else if (page == 1) {
     switchingFunction('registerPage', false);
     switchingFunction('loginPage', true);
     switchingFunction('mainPage', false);
   }
-  else if(page == 2){
+  else if (page == 2) {
     switchingFunction('registerPage', true);
     switchingFunction('loginPage', false);
     switchingFunction('mainPage', false);
   }
 }
 
-function switchingFunction(className, visibility){
-  if (visibility){
+function switchingFunction(className, visibility) {
+  if (visibility) {
     document.querySelector(`.${className}`).classList.replace('d-none', 'd-default')
   }
-  else{
+  else {
     document.querySelector(`.${className}`).classList.replace('d-default', 'd-none')
   }
 }
 
-document.querySelector("#loginTag").addEventListener("click", function(){
-    switchPage(1);
+document.querySelector("#loginTag").addEventListener("click", function () {
+  switchPage(1);
 })
-document.querySelector("#homeTag").addEventListener("click", function(){
-    switchPage(0);
+document.querySelector("#homeTag").addEventListener("click", function () {
+  switchPage(0);
 })
-document.querySelector("#registerTag").addEventListener("click", function(){
-    switchPage(2);
+document.querySelector("#registerTag").addEventListener("click", function () {
+  switchPage(2);
 })
 
 
